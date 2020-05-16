d8e073c44997645ca2bef26b7a49ea5cb1c7861f
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-05-16 12:40:25 -0400

    Add NXP TapLinx NFC Mifare SDK

--- IoTSensor/SinkSensor/app/src/main/java/dev/terrell/sinksensor/MainActivity.java
+++ IoTSensor/SinkSensor/app/src/main/java/dev/terrell/sinksensor/MainActivity.java
@@ -2,13 +2,59 @@ package dev.terrell.sinksensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.nxp.nfclib.NxpNfcLib;
+import com.nxp.nfclib.CardType;
 
 public class MainActivity extends AppCompatActivity {
+    private NxpNfcLib nfc;
+    private String LogTag = "SinkSensor";
+    private void initNFC()
+    {
+        nfc = NxpNfcLib.getInstance();
+        try {
+            nfc.registerActivity(this, Secrets.MifareKey);
+        }catch (Exception e){
+            Log.e(LogTag, e.toString());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initNFC();
+    }
+
+    @Override
+    protected void onResume ()
+    {
+        nfc.startForeGroundDispatch();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        nfc.stopForeGroundDispatch();
+        super.onPause();
+    }
+
+    @Override
+    public void onNewIntent(final Intent intent)
+    {
+        Log.d(LogTag, "Intent Received");
+        detectNFC(intent);
+        super.onNewIntent(intent);
+    }
+
+    private void detectNFC(final Intent intent)
+    {
+        CardType cardType = nfc.getCardType(intent);
+        Log.d(LogTag, "Card Type Detected: " + cardType.getTagName());
     }
 }
