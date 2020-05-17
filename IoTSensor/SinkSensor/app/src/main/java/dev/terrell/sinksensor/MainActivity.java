b1ba3a4b8e04af942c13c0e95026768f61f5b623
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-05-17 00:02:22 -0400

    Add GuestID and Last Scan Time for SinkSensor

--- IoTSensor/SinkSensor/app/src/main/java/dev/terrell/sinksensor/MainActivity.java
+++ IoTSensor/SinkSensor/app/src/main/java/dev/terrell/sinksensor/MainActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "SinkSensor";
     private NFC nfc;
     private BLE ble;
+    private TextView guestText;
+    private TextView scanText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nfc = NFC.getInstance(this);
         ble = BLE.getInstance(this.getApplicationContext());
+        guestText = (TextView) findViewById(R.id.guestText);
+        scanText = (TextView) findViewById(R.id.scanText);
     }
 
     @Override
@@ -39,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.w(TAG, "Unable to Read Tag");
         }
+        guestText.setText("GuestID: " + guestIdentity);
+        Date currentTime = Calendar.getInstance().getTime();
+        scanText.setText("Last Scan: " + currentTime.toString());
         super.onNewIntent(intent);
     }
 }
